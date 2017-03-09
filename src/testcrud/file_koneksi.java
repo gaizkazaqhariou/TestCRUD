4d9e2c6a659a94e7154ca1d81f66ebb187d03376
gaizkazaqhariou <muhammad_zaqhariou_24rpl@student.smktelkom-mlg.sch.id>
2017-03-09 16:47:05 +0700

    first commit

---
+++ src/testcrud/file_koneksi.java
@@ -0,0 +1,19 @@
+package testcrud; 
+ 
+import com.mysql.jdbc.Connection; 
+import com.mysql.jdbc.Driver; 
+import java.sql.DriverManager; 
+import java.sql.SQLException; 
+ 
+public class file_koneksi { 
+ 
+    private static Connection koneksi; 
+    public static Connection GetConnection() throws SQLException 
+{ 
+        if (koneksi == null) { 
+            Driver driver = new Driver(); 
+            koneksi =(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/db_testkoneksi?zeroDateTimeBehavior=convertToNull", "root", ""); 
+        } 
+        return koneksi; 
+    } 
+    } 
